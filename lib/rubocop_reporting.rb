60e48800e01130a32de5ae3e1c886889d01069b5
Yoga Hapriana <thenelse@rocketmail.com>
2018-03-22 13:54:44 +0700

    Fixing version 0.1.0

--- lib/rubocop_reporting.rb
+++ lib/rubocop_reporting.rb
@@ -2,9 +2,10 @@
 
 require 'rubocop_reporting/version'
 require 'json'
+require 'csv'
 
 module RubocopReporting
-  class CSV
+  class ToCSV
     def initialize(path_to_file = nil)
       @path_to_file = path_to_file
     end
@@ -22,11 +23,33 @@ module RubocopReporting
         raise FileNotSupported
       end
 
-      JSON.parse(file)
+      generate_csv_file(file)
     end
 
     def accepted_formats
       ['.json']
+    end
+
+    def generate_csv_file(file)
+      result = processing_json(file)
+      CSV.open("#{Dir.pwd}/rubocop_reporting.csv", 'wb') do |csv|
+        csv << result.keys
+        csv << result.values
+      end
+    end
+
+    def processing_json(file)
+      result = {}
+      json = JSON.parse(file)
+
+      json['files'].each do |json_file|
+        json_file['offenses'].each do |offense|
+          cop_name = offense['cop_name'].split('/')[0]
+          result[cop_name] = result[cop_name].nil? ? 1 : (result[cop_name] + 1)
+        end
+      end
+
+      result
     end
   end
 end
--- spec/rubocop_reporting_spec.rb
+++ spec/rubocop_reporting_spec.rb
@@ -6,24 +6,33 @@ RSpec.describe RubocopReporting do
   end
 
   it do
-    rubocop_reporting = RubocopReporting::CSV.new
+    rubocop_reporting = RubocopReporting::ToCSV.new
     expect { rubocop_reporting.generate }.to raise_error(FileNotFound)
   end
 
   it do
-    rubocop_reporting = RubocopReporting::CSV.new('path')
+    rubocop_reporting = RubocopReporting::ToCSV.new('path')
     expect { rubocop_reporting.generate }.to raise_error(FileNotFound)
   end
 
   it do
     path = "#{Dir.pwd}/spec/files/wrong_file.txt"
-    rubocop_reporting = RubocopReporting::CSV.new(path)
+    rubocop_reporting = RubocopReporting::ToCSV.new(path)
     expect { rubocop_reporting.generate }.to raise_error(FileNotSupported)
   end
 
   it do
     path = "#{Dir.pwd}/spec/files/rubocop_wrong_content.json"
-    rubocop_reporting = RubocopReporting::CSV.new(path)
+    rubocop_reporting = RubocopReporting::ToCSV.new(path)
     expect { rubocop_reporting.generate }.to raise_error(JSON::ParserError)
+  end
+
+  it do
+    path = "#{Dir.pwd}/spec/files/rubocop.json"
+    rubocop_reporting = RubocopReporting::ToCSV.new(path)
+    rubocop_reporting.generate
+    path = "#{Dir.pwd}/rubocop_reporting.csv"
+    expect(File).to exist(path)
+    # FileUtils.rm(path)
   end
 end
